5f922adf05047b72895bced9b979f6d8857bb3e1
student411300467 <magicmeijun@gmail.com>
2025-09-22 15:01:50 +0800

    第七次提交 (螢幕觸控)

--- app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
@@ -1,26 +1,30 @@
 package tw.edu.pu.csim.tcyang.lotto
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import tw.edu.pu.csim.tcyang.lotto.ui.theme.LottoTheme
-import android.widget.Button
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.ui.Alignment
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import tw.edu.pu.csim.tcyang.lotto.ui.theme.LottoTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,20 +48,29 @@ fun Play(modifier: Modifier = Modifier) {
         mutableStateOf((1..100).random())
     }
 
-    Column (modifier = modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally)
-    {
-        Text(
-            text = "樂透數字(1-100)為 $lucky",
-            modifier = modifier
-        )
+    val context = LocalContext.current
 
-        Button(
-            onClick = { lucky = (1..100).random() }
-        )
-        {
-            Text("重新產生樂透碼")
+    Box {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .clickable {
+                    Toast.makeText(context, text = "螢幕觸控", duration = Toast.LENGTH_LONG).show()
+                },
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement . Center)
+            {
+            Text(
+                text = "樂透數字(1-100)為 $lucky",
+                modifier = modifier
+            )
+
+            Button(
+                onClick = { lucky = (1..100).random() }
+            )
+            {
+                Text("重新產生樂透碼")
+            }
         }
     }
 }
